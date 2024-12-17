9d3700c970481b33bb0e6d391755e4157bc9139a
denizzmazarovv <81916809+denizzmazarovv@users.noreply.github.com>
2024-12-17 21:21:52 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,7 @@
-# from aiogram import Bot, Dispatcher, types
-# from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-# from aiogram.utils import executor
-
-# # Вставь сюда свой токен от BotFather
-# TOKEN = "7197694845:AAHrF6OeDcNoWDdFghKch03cpigu2wQy2f8"
-
-# bot = Bot(token=TOKEN)
-# dp = Dispatcher(bot)
-
-# # Команда /start
-# @dp.message_handler(commands=["start"])
-# async def start_command(message: types.Message):
-#     # Создаём клавиатуру с кнопкой для запуска мини-приложения
-#     keyboard = InlineKeyboardMarkup()
-#     keyboard.add(
-#         InlineKeyboardButton(
-#             text="Открыть MiniApp",
-#             web_app=WebAppInfo(url="http://127.0.0.1:5000/")  # URL твоего приложения
-#         )
-#     )
-#     await message.answer("Нажми на кнопку, чтобы открыть MiniApp:", reply_markup=keyboard)
-
-# if __name__ == "__main__":
-#     executor.start_polling(dp)
-# -------------------------------------------------------------------------------
 import os
+import logging
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-import logging
 import asyncio
 
 # Чтение токена из переменной окружения
@@ -37,7 +11,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: Message):
@@ -47,7 +21,7 @@ async def send_welcome(message: Message):
 async def on_start():
     try:
         # Старт polling
-        await dp.start_polling()
+        await dp.start_polling(bot)
     except Exception as e:
         logging.error(f"Error occurred: {e}")
 
